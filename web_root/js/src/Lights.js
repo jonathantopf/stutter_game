e1b5a8f7494718d87017e7b5aec94eee2d0fac10
Jonathan Topf <jonathan@jonathantopf.com>
2014-07-22 13:40:39 +0100

    lights now fade up

--- web_root/js/src/Lights.js
+++ web_root/js/src/Lights.js
@@ -1,7 +1,7 @@
 // copyright Jonathan Topf 2014
 
 // ----------------------------------------------------------------------------------------------------
-// Lights
+// Light Rig constructor
 // ----------------------------------------------------------------------------------------------------
 
 var Lights = {}
@@ -9,14 +9,41 @@ var Lights = {}
 Lights.Rig = function ()
 {
     // create lights
-    this.key_light = new THREE.PointLight( 0xFFFFFF, 1);
+    this.lights = 
+    {
+        key : 
+        {
+            light: new THREE.PointLight( 0xFFFFFF, 0),
+            target_intensity : 1,
+            speed: 0.005
+        }
+    }
+
+    console.log(new THREE.PointLight( 0xFFFFFF, 1));
 
     // add to scene
     this.scene_object = new THREE.Object3D();
-    this.scene_object.add(this.key_light);
+    this.scene_object.add(this.lights.key.light);
 
     // position lights
-    this.key_light.position.x = -100;
-    this.key_light.position.y = 100;
-    this.key_light.position.z = 130;
+    this.lights.key.light.position.x = -100;
+    this.lights.key.light.position.y = 100;
+    this.lights.key.light.position.z = 130;
 }
+
+
+// ----------------------------------------------------------------------------------------------------
+// Light Rig update method
+// ----------------------------------------------------------------------------------------------------
+
+Lights.Rig.prototype.update = function (update)
+{
+    for (var key in this.lights)
+    {
+        if (this.lights[key].light.intensity < this.lights[key].target_intensity)
+        {
+            this.lights[key].light.intensity += this.lights[key].speed;
+        }
+    }
+}
+

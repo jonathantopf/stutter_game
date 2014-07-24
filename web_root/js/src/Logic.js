3bd4446ca6d9a1374c6b1da5f94154bb3c1395c7
Jonathan Topf <jonathan@jonathantopf.com>
2014-07-24 16:23:31 +0100

    added extra test script content

--- web_root/js/src/Logic.js
+++ web_root/js/src/Logic.js
@@ -20,17 +20,33 @@ Logic.init = function ()
         new Commands.Wait           ('loading', Logic.loaiding_indicator),
         new Commands.Lights         (null,      Logic.light_rig.lights['key'], {value : 1, speed : 0.01}),
         new Commands.Sleep          (null,      1),
-        new Commands.Speak          (null,      'H... H... Hi!\ncould you give me \na hand?\n', Logic.text_buffer),
-        new Commands.Sleep          (null,      2),
+        new Commands.Speak          (null,      'H...H... Hi!\ncould you give me \na hand?', Logic.text_buffer),
+        new Commands.Sleep          (null,      1.3),
+
         new Commands.ClearBuffer    (null,      Logic.text_buffer),
         new Commands.Speak          (null,      'I\'m going shopping\nbut its kinda hard\nwith this\ns...s...s...', Logic.text_buffer),
         new Commands.Speak          (null,      'stutter', Logic.text_buffer, {jitter_mult : 3}),
         new Commands.Sleep          (null,      3),
+
         new Commands.ClearBuffer    (null,      Logic.text_buffer),
-        new Commands.Speak          (null,      'The only thing\nthat helps is if I\n', Logic.text_buffer),
-        new Commands.Speak          (null,      'sing', Logic.text_buffer, {color : 0xffff00, cycle_color : true}),
-        new Commands.Sleep          (null,      6),
+        new Commands.Speak          (null,      'The only thing\nthat helps is if I\ns...s...s... ', Logic.text_buffer),
+        new Commands.Speak          (null,      'sing', Logic.text_buffer, {color : 0xffff55, cycle_color : true, sparkle : true}),
+        new Commands.Sleep          (null,      4),
+
+        new Commands.ClearBuffer    (null,      Logic.text_buffer),
+        new Commands.Speak          (null,      'F...First\nI\'ll whistle a tune', Logic.text_buffer),
+        new Commands.Sleep          (null,      2),
+
+        new Commands.ClearBuffer    (null,      Logic.text_buffer),
+        new Commands.Speak          (null,      'then when I give\nyou the sign\nyou repeat it\nusing your keybord', Logic.text_buffer),
+        new Commands.Sleep          (null,      2),
+
+        new Commands.ClearBuffer    (null,      Logic.text_buffer),
+        new Commands.Speak          (null,      'like this...', Logic.text_buffer),
+        new Commands.Sleep          (null,      2),
+
         new Commands.Lights         (null,      Logic.light_rig.lights['key'], {value : 0, speed : 0.02}),
+        new Commands.ClearBuffer    (null,      Logic.text_buffer),
         new Commands.End            ('end')
     ]
 

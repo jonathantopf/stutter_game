3fc61097dba3997897d5f488250549103ef9928e
Jonathan Topf <jonathan@jonathantopf.com>
2014-08-04 17:17:15 +0100

    added extra testing logic

--- web_root/js/src/Logic.js
+++ web_root/js/src/Logic.js
@@ -41,21 +41,29 @@ Logic.init = function ()
         new Commands.Speak          (null,           'then when I give\nyou the sign\nyou repeat it\nusing your keybord', Logic.text_buffer, Logic.singer),
         new Commands.Sleep          (null,           2),
      
-        new Commands.ClearBuffer    (null,           Logic.text_buffer),
-        new Commands.Speak          (null,           'like this...', Logic.text_buffer, Logic.singer),
-        new Commands.Sleep          (null,           2),
-
-        new Commands.ClearBuffer    ('example_1',    Logic.text_buffer),    
-        new Commands.SongDemo       (null,           Sound.songs['testing'], Logic.singer, 300),
-        new Commands.Sleep          (null,           2),
+        new Commands.ClearBuffer    (null,             Logic.text_buffer),
+        new Commands.Speak          (null,             'like this...', Logic.text_buffer, Logic.singer),
+        new Commands.Sleep          (null,             1.2),
   
-        new Commands.ABSwitch       ('ab',           Logic.text_buffer, 'give it a go', 'song_1', 'show me again', 'example_1'),
-        new Commands.ClearBuffer    ('song_1',       Logic.text_buffer),
+        new Commands.ClearBuffer    ('example_1',      Logic.text_buffer),    
+        new Commands.SongDemo       (null,             Sound.songs['testing'], Logic.singer, 300),
+        new Commands.Sleep          (null,             2),
+        new Commands.ABSwitch       (null,             Logic.text_buffer, 'give it a go', 'song_1', 'show me again', 'example_1'),
+  
+        new Commands.ClearBuffer    ('song_1',         Logic.text_buffer),
+        new Commands.Speak          (null,             'Ok lets go\nReady when you are', Logic.text_buffer, Logic.singer),
+        new Commands.SongTest       (null,             Logic.text_buffer, Sound.songs['testing'], Logic.singer, 'song_1_success', 'song_1_fail'),
+  
+        new Commands.ClearBuffer    ('song_1_fail',    Logic.text_buffer),
+        new Commands.Speak          (null,             'So close!, lets try again', Logic.text_buffer, Logic.singer),
+        new Commands.GoTo           (null,             'song_1'),
 
-        new Commands.SongTest       (null,           Logic.text_buffer, Sound.songs['testing'], Logic.singer, 'lights_down', 'ab'),
+        new Commands.ClearBuffer    ('song_1_success', Logic.text_buffer),
+        new Commands.Speak          (null,             'Amazing!\nThanks', Logic.text_buffer, Logic.singer),
+        new Commands.Sleep          (null,             3),
 
-        new Commands.Lights         ('lights_down',  Logic.light_rig.lights['key'], {value : 0, speed : 0.02}),
-        new Commands.ClearBuffer    (null,           Logic.text_buffer),
+        new Commands.Lights         ('lights_down',    Logic.light_rig.lights['key'], {value : 0, speed : 0.02}),
+        new Commands.ClearBuffer    (null,             Logic.text_buffer),
         new Commands.End            ('end')   
     ]
 
